6e660c3c284af8c8f1a98547febba1ee56869c0f
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2025-02-20 21:38:53 +0530

    Chore: commented dummy

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       <Container>
         <Hero />
         {/* <Hero /> */}
-        <ToDelete />
+        {/* <ToDelete /> */}
       </Container>
     </Section>
   );
