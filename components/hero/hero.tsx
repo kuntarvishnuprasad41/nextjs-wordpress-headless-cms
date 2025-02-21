f1d2fa43a5d7361fbb9216cd9abde5c13e5c778c
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2025-02-21 08:15:43 +0530

    chore: 1. Search bar centering 2. Keeping everything inside screen-w 3. Making carousel overflow on desktop

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -21,7 +21,7 @@ export default function Hero() {
   return (
     <div
       className="min-h-screen  p-0  bg-gradient-to-b from-black via-gray-950 to-white
- text-white"
+ text-white  overflow-hidden"
     >
       {/* Hero Section */}
       <section className="container mx-auto px-8 py-8 max-w-10xl">
@@ -58,7 +58,7 @@ export default function Hero() {
       <NavTabs />
 
       {/* Articles Carousel */}
-      <section className="container mx-auto px-5 py-8 overflow-hidden ">
+      <section className="container mx-auto px-5 py-8 overflow-visible  scrollbar-hide">
         <Carousel
           opts={{
             align: "start",
--- components/searchTabs/NavTabs.tsx
+++ components/searchTabs/NavTabs.tsx
@@ -32,21 +32,23 @@ function NavTabs() {
 
   return (
     <div className="max-w-10xl sm:md:max-w-sm w-full border-gray-800 overflow-hidden">
-      <div className="container mx-auto md:px-5 pr-0 pl-5 py-4">
+      <div className="container mx-auto md:px-5 pr-0 px-5 py-4">
         <div className="flex flex-col md:flex-row gap-10 items-center justify-between">
-          <div className="flex items-center w-full max-w-[800px] p-2 sm:p-0 bg-white bg-opacity-[10%] rounded-full shadow-md outline-none">
-            <Input
-              type="text"
-              placeholder="How can I help you?"
-              className="flex-1 bg-transparent border-none text-white placeholder-gray-400 focus:outline-none focus-visible:outline-none focus:ring-0 focus:border-transparent"
-            />
-            <Button
-              size="icon"
-              variant="ghost"
-              className="text-black rounded-full bg-white"
-            >
-              <ArrowUp className="w-5 h-5" />
-            </Button>
+          <div className="px-[24px] w-full pl-1">
+            <div className="flex items-center w-full max-w-[800px] p-2 sm:p-0 bg-white bg-opacity-[10%] rounded-full shadow-md outline-none ">
+              <Input
+                type="text"
+                placeholder="How can I help you?"
+                className="flex-1 bg-transparent border-none text-white placeholder-gray-400 focus:outline-none focus-visible:outline-none focus:ring-0 focus:border-transparent"
+              />
+              <Button
+                size="icon"
+                variant="ghost"
+                className="text-black rounded-full bg-white"
+              >
+                <ArrowUp className="w-5 h-5" />
+              </Button>
+            </div>
           </div>
 
           <div
