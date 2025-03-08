f601930b7830843209e4c43a9f9115b3c2203eb5
ridhav30 <ridhav30@gmail.com>
2025-03-08 09:58:02 +0530

    updated thinngs

--- app/[slug]/page.jsx
+++ app/[slug]/page.jsx
@@ -1,7 +1,7 @@
 import { fetchDataFn } from "@/lib/fetch";
 export default async function Page({ params }) {
     const { slug } = await params
-    const data = await fetchDataFn(`/wp-json/wp/v2/parent-category-posts?parent_slug=/${slug}`)
+    const data = await fetchDataFn(`/wp-json/wp/v2/parent-category-posts?parent_slug=${slug}`)
     return <div>
         {JSON.stringify(data)}
     </div>
