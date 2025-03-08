31ad070795ffb6ebddcda415a03caf16152af7be
ridhav30 <ridhav30@gmail.com>
2025-03-08 13:56:49 +0530

    working as expected

--- app/[slug]/page.jsx
+++ app/[slug]/page.jsx
@@ -1,12 +1,30 @@
 import { fetchDataFn } from "@/lib/fetch";
 import { Section, Container } from "@/components/craft";
 const descriptions = {
-    products: "Revolutionary Smart Home Device Launches: Enhance Security, Save Energy, and Simplify Life with Cutting-Edge Technology.",
-    enterprise: "Transform Your Business with Scalable Enterprise Solutions: Boost Efficiency, Innovation, and Growth Across All Operations.",
-    smes: "Empowering SMEs: Affordable Tools to Streamline Operations, Drive Productivity, and Compete in the Digital Economy.",
-    startups: "Fuel Your Startup's Success: Cutting-Edge Resources to Accelerate Growth, Innovation, and Market Reach.",
-    'women-entrepreneurs': "Supporting Women Entrepreneurs: Tools, Mentorship, and Networks to Break Barriers and Achieve Business Excellence.",
-    'gen-new': "Inspiring the Next Generation: Innovative Platforms to Foster Creativity, Leadership, and Entrepreneurial Spirit in Young Minds."
+    products: {
+        name: "products",
+        description: "Revolutionary Smart Home Device Launches: Enhance Security, Save Energy, and Simplify Life with Cutting-Edge Technology."
+    },
+    enterprise: {
+        name: "enterprise",
+        description: "Transform Your Business with Scalable Enterprise Solutions: Boost Efficiency, Innovation, and Growth Across All Operations."
+    },
+    smes: {
+        name: "SMEs",
+        description: "Empowering SMEs: Affordable Tools to Streamline Operations, Drive Productivity, and Compete in the Digital Economy."
+    },
+    startups: {
+        name: "startups",
+        description: "Fuel Your Startup's Success: Cutting-Edge Resources to Accelerate Growth, Innovation, and Market Reach."
+    },
+    'women-entrepreneurs': {
+        name: "Women Entrepreneurs",
+        description: "Supporting Women Entrepreneurs: Tools, Mentorship, and Networks to Break Barriers and Achieve Business Excellence."
+    },
+    'gen-new': {
+        name: "Gen New",
+        description: "Inspiring the Next Generation: Innovative Platforms to Foster Creativity, Leadership, and Entrepreneurial Spirit in Young Minds."
+    }
 }
 export default async function Page({ params }) {
     const { slug } = await params
@@ -16,11 +34,11 @@ export default async function Page({ params }) {
     return <Section>
         <Container>
             <section className="container px-8 py-8 max-w-10xl flex flex-col gap-4">
-                <h1 className="text-center capitalize text-4xl font-bold">{data?.name || slug}</h1>
+                <h1 className="text-center capitalize text-4xl font-bold">{data?.name || descriptions[slug]?.name}</h1>
                 <div className="mx-auto" style={{
                     width: "570px"
                 }}>
-                    <p className="text-center">{descriptions[slug]}</p>
+                    <p className="text-center">{descriptions[slug]?.description}</p>
                 </div>
             </section>
         </Container>
