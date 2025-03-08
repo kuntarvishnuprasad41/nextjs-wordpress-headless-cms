ad3a77131d7b213c45a865a8af6c1764db8b2dae
ridhav30 <ridhav30@gmail.com>
2025-03-08 09:37:47 +0530

    updated the data

--- app/[slug]/page.jsx
+++ app/[slug]/page.jsx
@@ -1,6 +1,8 @@
+import { fetchDataFn } from "@/lib/fetch";
 export default async function Page({ params }) {
     const { slug } = await params
+    const data = await fetchDataFn(`/wp-json/wp/v2/parent-category-posts?parent_slug=/${slug}`)
     return <div>
-        {slug}
+        {JSON.stringify(data)}
     </div>
 }
