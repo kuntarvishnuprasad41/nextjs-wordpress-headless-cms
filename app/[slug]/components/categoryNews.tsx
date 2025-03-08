281520aad0a6518225a908b94eecc36c6ecf04b8
ridhav30 <ridhav30@gmail.com>
2025-03-08 15:08:54 +0530

    updating

---
+++ app/[slug]/components/categoryNews.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image"
+import Link from "next/link"
+export const NewsHeadings = ({ news, category }: { news: any, category?: string | undefined }) => {
+    const [firstItem] = news
+    return <div className="flex flex-col gap-2">
+        {category && <h1 className="uppercase text-black/60">{category}</h1>}
+        <div className="flex flex-col gap-2">
+            <Link href={"#"}>
+                <Image src={firstItem?.featured_image?.url} alt="Description Image" width={200} height={160}
+                    className="w-[100%] rounded-xl h-[200px] object-cover"
+                />
+            </Link>
+            {
+                news.length > 0 && news.slice(0, 3).map((item: any, index: number) => {
+                    return <Link href={"#"} key={index} className="font-bold md:text-xl break-all border-b pb-2">
+                        <h1>
+                            {item.title}
+                        </h1>
+                    </Link>
+                })
+            }
+        </div>
+    </div>
+}
+export function CategoryNews({ data }: { data: any }) {
+    const { categories } = data
+    return <div className="flex flex-col mx-auto gap-[1px] p-2">
+        <div>
+            <span className="px-3 py-1 rounded-full capitalize bg-black text-white">{data?.name}</span>
+        </div>
+        <div className="border-t border-t-black py-4 grid grid-cols-1 md:grid-cols-4 gap-7">
+            {categories.length > 0 && categories.slice(0, 3).map((item: any) => {
+                return <NewsHeadings key={item?.id} category={item.name} news={item.posts} />
+            })}
+            <div>
+                <Image alt="ad" className="md:w-full w-[300px] mx-auto md:mx-0 h-[450px] md:h-full object-cover" src={"https://wordpress.sscinitiatives.com/wp-content/uploads/2025/02/ad-section-one.png"} width={274} height={462} />
+            </div>
+        </div>
+
+    </div>
+}
