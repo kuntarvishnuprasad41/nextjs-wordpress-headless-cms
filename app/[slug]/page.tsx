fba355fcebf97962568dc6238156407c8c924f72
ridhav <ridhav30@gmail.com>
2025-03-10 02:16:10 +0530

    updated as tsx

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import { fetchDataFn } from "@/lib/fetch";
 import { Section, Container } from "@/components/craft";
 import { CategoryNewsSectionOne, CategoryNewsSectionTwo } from "./components/categoryNews"
-const descriptions = {
+const descriptions:any = {
     products: {
         name: "products",
         description: "Revolutionary Smart Home Device Launches: Enhance Security, Save Energy, and Simplify Life with Cutting-Edge Technology."
@@ -27,22 +27,25 @@ const descriptions = {
         description: "Inspiring the Next Generation: Innovative Platforms to Foster Creativity, Leadership, and Entrepreneurial Spirit in Young Minds."
     }
 }
-export default async function Page({ params }) {
+export default async function Page({ params }: { params: any }) {
     const { slug } = await params
     const data = await fetchDataFn(`/wp-json/wp/v2/parent-category-posts?parent_slug=${slug}`)
     const { categories = [] } = data
-    const [firstCategory = {}, secondCategory, thirdCategory,...restCategories] = categories
+    const [firstCategory = {}, secondCategory, thirdCategory, ...restCategories] = categories
     return <Section>
         <Container>
             <section className="container px-8 py-8 max-w-10xl flex flex-col gap-4">
                 <h1 className="md:text-center capitalize text-2xl md:text-4xl font-bold px-2">{data?.name || descriptions[slug]?.name}</h1>
-                <div className="md:mx-auto w-full px-2" style={{
-                    maxWidth: "570px"
-                }}>
+                <div className="md:mx-auto w-full md:w-[570px] px-2">
                     <p className="md:text-center">{descriptions[slug]?.description}</p>
                 </div>
                 <CategoryNewsSectionOne data={firstCategory} />
                 <CategoryNewsSectionTwo data={[secondCategory, thirdCategory]} />
+                <div className="min-h-screen  p-0  bg-gradient-to-b from-[#13195b] via-[#3e4677] to-white" style={{
+
+                }}>
+
+                </div>
                 <CategoryNewsSectionTwo data={restCategories} />
             </section>
         </Container>
