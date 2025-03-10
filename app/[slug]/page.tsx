884ab5fa6e8ab6f0337c9e20f60e766e63adec06
ridhav <ridhav30@gmail.com>
2025-03-10 13:15:05 +0530

    updated all

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -44,27 +44,6 @@ export default async function Page({ params }: { params: any }) {
             <section className="container px-8 py-8 max-w-10xl">
                 <CategoryNewsSectionTwo data={[secondCategory, thirdCategory]} />
             </section>
-            <div className="min-h-screen  p-8  bg-gradient-to-b from-[#13195b] via-[#3e4677] to-white text-white">
-                <section className="container px-10 py-8 max-w-10xl flex flex-col gap-4">
-                    <h1 className="text-4xl font-semibold">Leading Videos</h1>
-                    <div className="md:w-[570px] text-gray-400">
-                        <p>Understand the perspectives of leaders and change-makers in business, politics and civil society.</p>
-                    </div>
-                    <div>
-                        <div className="group relative w-[276px] h-[400px] rounded-xl overflow-hidden">
-                            <iframe
-                                className="w-full h-full rounded-xl object-fill"
-                                src="https://www.youtube.com/embed/AGYv0nphF6k?autoplay=1&mute=1&enablejsapi=1"
-                                allow="autoplay"
-                                id="video"
-                            ></iframe>
-                            <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 group-hover:opacity-0 transition-opacity">
-                                <p className="text-white text-lg">Hover to play</p>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-            </div>
             <section className="container px-8 py-8 max-w-10xl">
                 <CategoryNewsSectionTwo data={restCategories} />
             </section>
